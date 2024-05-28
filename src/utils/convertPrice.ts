a920274c1012081181b4c5bd6051e931a8429bbc
Leonardo <leonardonabarro2003@gmail.com>
2024-05-28 10:12:05 -0300

    feat: fetech home products

---
+++ src/utils/convertPrice.ts
@@ -0,0 +1,10 @@
+export function convertPrice(value: number) {
+  const price = value.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  })
+
+  return price
+}
